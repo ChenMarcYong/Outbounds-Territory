ec9b18baa94348b681d77ebdf4ec03953ead95cf
chenmarcyong@gmail.com <chenmarcyong@gmail.com>
2025-03-02 20:43:28 +0100

    add spaceship

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,29 +11,58 @@ public class PlayerController : MonoBehaviour
 
     
     public  InputAction playerControls;
-    public float moveSpeed = 1.0f;
+    public float moveSpeed = 25.0f;
+    
     public InputActionReference move;
+    public InputActionReference ascend;
+    public InputActionReference rotate;
 
     private Vector2 moveDirection = Vector2.zero;
+    private float ascendSpeed = 5.0f;
     private Rigidbody rb;
+
+
+    public float verticalMove;
+    public float horizontalMove;
+    public float mouseInputX;
+    public float mouseInputY;
+    public float rollInput;
+
+    [SerializeField]
+    float speedMult = 1.0f;
+
+    [SerializeField]
+    float speedMultAngle = 0.5f;
+
+    [SerializeField]
+    float speedRollMultAngle = 0.05f;
 
     void Start()
     {
+
+        Cursor.lockState = CursorLockMode.Locked;
+
+
         rb = GetComponent<Rigidbody>();
-        //playerControls = GetComponent<InputAction>();
         rb.useGravity = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
         moveDirection = move.action.ReadValue<Vector2>();
+        ascendSpeed = ascend.action.ReadValue<float>();
+        UnityEngine.Debug.Log(rb.position);
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        rb.velocity = new Vector3(- moveDirection.x * moveSpeed, ascendSpeed * moveSpeed, - moveDirection.y * moveSpeed) ;
     }
+
+    /*public void OnMove() 
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }*/
 
 
     private void OnEnable()
